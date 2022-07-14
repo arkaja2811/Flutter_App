65629a493342a5bf9ebbff0bc5f1ebd6acccf451
arkaja2811 <sharanarkaja@gmail.com>
2022-07-14 20:20:47 +0530

    Stage-4 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:e_commerce/pages/login_page.dart';
 import 'package:flutter/material.dart';
+//import 'package:google_fonts/google_fonts.dart';
+
 
 import 'pages/home_page.dart';
 
@@ -16,7 +18,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       //home: HomePage(), 
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        //fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(brightness: Brightness.dark),
       //initialRoute: "/home",
       routes: {
