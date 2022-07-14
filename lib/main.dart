be4046451b1325210db424621bd12e4993ad72e8
arkaja2811 <sharanarkaja@gmail.com>
2022-07-14 14:02:50 +0530

    Stage-3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:e_commerce/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
-import 'home_page.dart';
+import 'pages/home_page.dart';
 
 void main()
 {
@@ -13,7 +14,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     
     return MaterialApp(
-      home: HomePage(), 
+      //home: HomePage(), 
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      //initialRoute: "/home",
+      routes: {
+        "/":(context)=>LoginPage(),
+        "/home":(context)=>HomePage(),
+        "/login":(context)=>LoginPage(),
+      },
     );
   }
+
+  /*bring_vegetables();
+  void bring_vegetables({@required bool thaila,int rupees=100}){
+    //Take Cycle
+    //GoTo Sector 16
+  }*/
 }
